1421d84658fbce1fdea2005b55c70d9e7e912591
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-04-10 07:46:58 -0300

    Properly generate sitemap

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -10,25 +10,25 @@ module Jekyll
     NORMAL_PRIORITY = 0.5
 
     def generate(site)
-      with_new_sitemap do |sitemap|
+      with_new_sitemap(site) do |sitemap|
         emit_urlset(sitemap, site)
       end
     end
-  
+
     private
-    def with_new_sitemap
-      File.open('sitemap.xml', 'w') do |sitemap_file|
+    def with_new_sitemap(site)
+      File.open("#{site.dest}sitemap.xml", 'w') do |sitemap_file|
         yield Builder::XmlMarkup.new(:target => sitemap_file, :indent => 2)
       end
     end
-  
+
     def emit_urlset(sitemap, site)
       sitemap.urlset(:xmlns => "http://www.sitemaps.org/schemas/sitemap/0.9") do |urlset|
         emit_root_url(urlset, site)
         site.pages.each { |page| emit_page_url(urlset, page, site) }
       end
     end
-  
+
     def emit_root_url(urlset, site)
       emit_new_url(urlset, site.config['url'], DAILY_CHANGE_FREQUENCY)
     end
@@ -36,7 +36,7 @@ module Jekyll
     def emit_page_url(urlset, post, site)
       emit_new_url(urlset, absolute_url(site, post), WEEKLY_CHANGE_FREQUENCY)
     end
-  
+
     def emit_new_url(urlset, location, change_frequency)
       urlset.url do |url|
         url.loc location
@@ -44,11 +44,11 @@ module Jekyll
         url.changefreq change_frequency
       end
     end
-    
+
     def absolute_url(site, page)
       site.config['url'] + page.url
     end
-  
+
     def simple_date_string(date)
       date.strftime("%Y-%m-%d")
     end
