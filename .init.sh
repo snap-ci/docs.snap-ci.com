75f01920e45325d81ff683e469aa8f7c8d7018b1
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-07-03 10:49:53 -0300

    Use the standard init script we use

--- .init.sh
+++ .init.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-set -x
+
+set -e
+
 BUNDLE_PATH=/tmp/.bundle-$(basename $(pwd))
-if [ -z "$BUNDLE_PATH" ]
+export BUNDLE_JOBS="${BUNDLE_JOBS:=4}"
+if [ ! -z "$SNAP_CI" ]
 then
-   BUNDLE_PATH=".bundle"
+  BUNDLE_PATH=$HOME/.bundle
 fi
-bundle install --local --path $BUNDLE_PATH --binstubs --clean
+
+mkdir -p $BUNDLE_PATH
+rm -rf vendor/bundle
+ln -sf $BUNDLE_PATH vendor/bundle
+
+export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
+
+while read line; do
+  [[ -z ${SNAP_CI} ]] && echo -ne "Doing $((C++)) things...\r"
+done < <(bundle check || bundle install --local --path vendor/bundle --binstubs --clean)
+
+echo
+echo "Done!"
