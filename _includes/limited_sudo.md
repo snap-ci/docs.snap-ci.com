3ceb4c672548c9b68dd10cb037e01a1bb4757b3f
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-07-04 12:35:30 -0300

    Improve the documentation on supported platforms

---
+++ _includes/limited_sudo.md
@@ -0,0 +1,1 @@
+> Snap provides limited sudo access. You can install your favourite package using `sudo yum install --assumeyes PACKAGE_NAME` if required.
--- _plugins/as_array.rb
+++ _plugins/as_array.rb
@@ -3,6 +3,14 @@ module Jekyll
     def as_array(input)
       input || []
     end
+
+    def monospaced_array_to_sentence_string(input)
+      array_to_sentence_string(as_array(input).collect {|i| "`#{i}`" })
+    end
+
+    def monospaced_array_to_bullet_list(input)
+      as_array(input).collect {|i| "* `#{i}`" }.join("\n")
+    end
   end
 end
 
