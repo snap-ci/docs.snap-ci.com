92687711eb99b7b4301e679e01a83399b848472f
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-08-29 16:39:42 -0300

    When linking to a draft page from a non-draft page, show an indicator on that link

--- lib/helpers/topic_link_tag_helper.rb
+++ lib/helpers/topic_link_tag_helper.rb
@@ -1,11 +1,18 @@
 module TopicLinkTagHelper
-  def topic_link(topic, text)
-    topics = html_resources.find_all do |r|
-      r.data.title.downcase == topic.downcase
+  def topic_link(topic_name, text)
+    topics = all_html_resources.find_all do |r|
+      r.data.title.downcase == topic_name.downcase
     end
     if topics.length != 1
-      raise "Expected 1 topic to match #{topic}, found #{topics.length} instead."
+      raise "Expected 1 topic to match #{topic_name}, found #{topics.length} instead."
     end
-    link_to text, topics.first
+
+    topic = topics.first
+
+    if topic.data.draft && !current_page.data.draft
+      link_to text, topic, :class => 'wip'
+    else
+      link_to text, topic
+    end
   end
 end
