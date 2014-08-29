6072ddc6d4bdef6bf10c301ae81c374bc2a95c89
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-08-29 16:39:04 -0300

    Extract a helper to provide a list of non-draft resources

--- lib/helpers/resources_helper.rb
+++ lib/helpers/resources_helper.rb
@@ -1,5 +1,9 @@
 module ResourcesHelper
   def html_resources
+    all_html_resources.find_all { |r| !r.data.draft }
+  end
+
+  def all_html_resources
     sitemap.resources.find_all {|r| r.content_type =~ %r{text/html} }
   end
 end
