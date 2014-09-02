1fc1d3f671488a81cc1553bc1b227cbdbc2e0ebe
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-09-02 10:07:54 +0530

    Add support for fragments in topic_link

--- lib/helpers/topic_link_tag_helper.rb
+++ lib/helpers/topic_link_tag_helper.rb
@@ -1,8 +1,11 @@
 module TopicLinkTagHelper
-  def topic_link(topic_name, text)
+  def topic_link(name, text)
+    topic_name, fragment = name.split('#')
+
     topics = all_html_resources.find_all do |r|
       r.data.title.downcase == topic_name.downcase
     end
+
     if topics.length != 1
       raise "Expected 1 topic to match #{topic_name}, found #{topics.length} instead."
     end
@@ -10,9 +13,9 @@ module TopicLinkTagHelper
     topic = topics.first
 
     if topic.data.draft && !current_page.data.draft
-      link_to text, topic, :class => 'wip'
+      link_to text, topic, :class => 'wip', :fragment => fragment
     else
-      link_to text, topic
+      link_to text, topic, :fragment => fragment
     end
   end
 end
