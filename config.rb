d8d8d9d5b4ebf174720ee05ac4c475f825e52361
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-09-09 08:26:21 +0530

    More redirects

--- config.rb
+++ config.rb
@@ -97,8 +97,11 @@ if ENV['S3_BUCKET']
     '/api/introduction/'                      => '/api/',
     '/databases/'                             => '/the-ci-environment/databases/',
     '/pipeline/introduction/'                 => '/pipeline/',
+    '/pipeline_clones_and_integration_pipelines/cloned_pipelines/' => '/working-with-branches/cloned-pipelines/',
+    '/pipeline_clones_and_integration_pipelines/integration_pipelines/' => '/working-with-branches/integration-pipelines/',
     '/supported_platforms/'                   => '/the-ci-environment/',
     '/the_ci_environment/the_ci_environment/' => '/the-ci-environment/',
+    'the_ci_environment/languages/overview/'  => '/the-ci-environment/languages/'
   }.each do |old_path, new_path|
     redirect old_path.dup, new_path.dup
   end
