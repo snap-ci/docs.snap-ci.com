dc80f929dff1bb259a326464a6057ea9b1c88d2c
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-09-22 13:13:42 +0530

    Automatically redirect in dev mode

--- lib/helpers/topic_link_tag_helper.rb
+++ lib/helpers/topic_link_tag_helper.rb
@@ -1,22 +1,38 @@
 module TopicLinkTagHelper
+  def topic_url(name)
+    url_for(topic_with_name(name))
+  end
+
   def topic_link(name, text, opts={})
-    topic_name, fragment = name.split('#', 2)
-    fragment = URI.encode(fragment) if fragment
+    topic = topic_with_name(name)
+    if topic.data.draft && !current_page.data.draft
+      link_to text, topic, :class => opts[:class] || 'wip', :fragment => fragment(name)
+    else
+      link_to text, topic, :class => opts[:class], :fragment => fragment(name)
+    end
+  end
+
+  private
+
+  def fragment(name)
+    name.split('#', 2)[1]
+  end
+
+  def topic_name(name)
+    name.split('#', 2)[0]
+  end
+
+  def topic_with_name(name)
+    fragment = URI.encode(fragment(name)) if fragment(name)
 
     topics = all_html_resources.find_all do |r|
-      r.data.title.downcase == topic_name.downcase
+      r.data.title.downcase == topic_name(name).downcase
     end
 
     if topics.length != 1
-      raise "Expected 1 topic to match #{topic_name}, found #{topics.length} instead."
+      raise "Expected 1 topic to match #{topic_name(name)}, found #{topics.length} instead."
     end
 
-    topic = topics.first
-
-    if topic.data.draft && !current_page.data.draft
-      link_to text, topic, :class => opts[:class] || 'wip', :fragment => fragment
-    else
-      link_to text, topic, :class => opts[:class], :fragment => fragment
-    end
+    topics.first
   end
 end
