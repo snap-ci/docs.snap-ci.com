f9412451828eb42b398ca40bb6bce4a715589bed
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-11-24 17:07:46 +0530

    Add a link to allow contribution to the documentation site

--- lib/helpers/resources_helper.rb
+++ lib/helpers/resources_helper.rb
@@ -1,4 +1,10 @@
 module ResourcesHelper
+
+  def edit_documentation_url
+    p = Pathname(current_page.source_file).relative_path_from(Pathname(root))
+    "https://github.com/snap-ci/docs.snap-ci.com/blob/master/#{p}"
+  end
+
   def html_resources
     all_html_resources.find_all { |r| !r.data.draft }
   end
