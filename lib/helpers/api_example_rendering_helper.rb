6e25faa4a28747e9476ac2d4481f254414e333a7
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-01-06 15:26:44 +0530

    Add documentation about API versioning.

--- lib/helpers/api_example_rendering_helper.rb
+++ lib/helpers/api_example_rendering_helper.rb
@@ -34,7 +34,7 @@ module ApiExampleRenderingHelper
     end
 
     curl_cmd << '-H'
-    curl_cmd << "'Accept: application/vnd.snap-ci.com.v1+json'"
+    curl_cmd << "'Accept: #{current_page.app.data.api.current_version}'"
 
     curl_cmd << options[:url]
 
