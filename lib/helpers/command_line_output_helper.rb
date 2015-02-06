3e008865623b7a1b7780c0f9f148f0a6d07a9cf6
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-02-06 11:43:03 +0530

    Improve chromedriver documentation with some howtos on how to use it from protractor

--- lib/helpers/command_line_output_helper.rb
+++ lib/helpers/command_line_output_helper.rb
@@ -5,16 +5,19 @@ module CommandLineOutputHelper
   def output_of_command(cmd, options={})
     # because `nginx` seems to work well for most CLI output!
     language           = options[:language] || options[:lang] || 'nginx'
+
+    code(language) do
+      stdout_and_stderr_of(cmd, options)
+    end
+  end
+
+  def stdout_and_stderr_of(cmd, options={})
     expected_exit_code = options[:expected_exit_code] || 0
 
     cmd << " 2>&1" unless cmd =~ /2>&1/
     output = %x[#{cmd}]
-
     raise "Expected exit status #{expected_exit_code} got #{$?.exitstatus} instead when executing `#{cmd}`" unless $?.exitstatus == expected_exit_code
-
-    code(language) do
-      output
-    end
-
+    output
   end
+
 end
